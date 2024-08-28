d85b4167413c07da7da8342766f4876763a2a43a
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-08-28 14:44:54 +0900

    Bottom Sheet가 움직이지 않도록 고정 및 Bottom Sheet의 Handler 뿐만 아니라 어느 부분을 드래그 해도  움직이도록 구현

--- src/components/Map/BannerContent.tsx
+++ src/components/Map/BannerContent.tsx
@@ -42,7 +42,11 @@ const BottomSheetContent = styled.div`
 `;
 
 const BottomSheetHandler = styled.div`
-  width: 100%;
+    z-index: 100;
+    position: absolute;
+    height: 100%;
+  
+    width: 100%;
   display: flex;
   justify-content: center;
 `;
